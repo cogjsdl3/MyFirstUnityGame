930b75d775c4ecd85560f0471247cff5fd0b3720
cogjsdl3 <80082555+cogjsdl3@users.noreply.github.com>
2021-03-05 12:18:30 +0900

    .

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player
+{
+    private string name;
+    private int level = 1;
+
+    public Player(string name) {
+        this.name = name;
+
+    }
+    public void GetInfo() {
+        Debug.Log("이름 : " + name);
+        Debug.Log("레벨 : " + level);
+    }
+    public void LevelUp()
+    {
+        this.level++;
+            Debug.Log("레벨 업~!");
+
+    }
+}
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player myplayer = new Player("홍길동");
+        myplayer.GetInfo();
+        myplayer.LevelUp();
+        myplayer.GetInfo();
+
+    }
+
+
+}
